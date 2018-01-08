e9552e1b475eca2281b1bdd3c75921474f204550
Maria Betances <mbetances1002@gmail.com>
2018-01-08 13:55:44 -0500

    updated solution for URLify

--- Chapter1/URLify.js
+++ Chapter1/URLify.js
@@ -2,9 +2,28 @@
 // You may assume that the string has sufficient space at the end to hold the additional characters,
 // and that you are given the 'true' length of the string.
 // BigO: O(n)
-const urlify = (str, num) => {
+// First Solution
+const urlify1 = (str, num) => {
   let updatedURL = str.trim().replace(/\s/g, '%20');
   return updatedURL;
 }
 
-urlify('today is   ', 11);
+urlify1('today is   ', 8);
+
+
+// Second Solution
+const urlify2 = (str, num) => {
+  let newStr = str.trim().split('');
+
+  let updatedStr = [];
+  for (let j = 0; j < newStr.length; j++) {
+    if (newStr[j] === ' ') {
+      updatedStr.push('%20');
+    } else {
+      updatedStr.push(newStr[j]);
+    }
+  }
+  return updatedStr.join('');
+}
+
+urlify2('today is Friday  ', 8);
