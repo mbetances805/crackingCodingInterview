6e3e7f832ae4cdb7b53aebcd066cd8874feb9bcc
Maria Betances <mbetances1002@gmail.com>
2018-01-08 14:19:34 -0500

    updated solution for palindromePermutation

--- Chapter1/palindromePermutation.js
+++ Chapter1/palindromePermutation.js
@@ -3,6 +3,7 @@
 // A permutation is a rearrangement of letters.
 // The palindrome does not need to be limited to just dictionary words.
 // Big O: O(n)
+// First Solution
 const palindromePermutation = (str) => {
   let newStr = str.split('').reverse().join('');
   if (str === newStr) {
@@ -13,3 +14,38 @@ const palindromePermutation = (str) => {
 }
 
 palindromePermutation('cat tac');
+
+// Second Solution
+const palindromePermutation2 = (str) => {
+  let strLength = str.length;
+  let hashTable = {};
+
+  for (let i = 0; i < strLength; i++) {
+    if (!hashTable[str[i]]) {
+      hashTable[str[i]] = 1;
+    } else {
+      let counter = hashTable[str[i]];
+      counter++;
+      hashTable[str[i]] = counter;
+    }
+  }
+
+  if (!(strLength % 2)) {
+    for (let letter in hashTable) {
+      if (hashTable[letter] % 2) {
+        return false;
+      }
+    }
+  }
+
+  for (let j = 0; j < strLength / 2; j++) {
+    if (j === 0 && str[j] !== str[strLength - 1]) {
+      return false;
+    } else if (j > 0 && str[j] !== str[strLength - 1 - j]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+palindromePermutation2('catymtac');
