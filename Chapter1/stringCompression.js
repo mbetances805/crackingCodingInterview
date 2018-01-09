35747e76826b5b6b214489dd072e7c1a231f1264
Maria Betances <mbetances1002@gmail.com>
2018-01-08 19:09:59 -0500

    updated stringCompression solution

--- Chapter1/stringCompression.js
+++ Chapter1/stringCompression.js
@@ -3,7 +3,8 @@
 // your method should return the original string.
 // You can assume the string has only uppercase and lowercase letters.
 
-const stringCompression = (str) => {
+// First Solution
+const stringCompression1 = (str) => {
   let compressedStr = '';
   let counter = 1;
   for (let i = 0; i < str.length; i++) {
@@ -22,4 +23,29 @@ const stringCompression = (str) => {
   }
 }
 
-stringCompression('abzzzzzzcd');
+stringCompression1('abzzzzzzcd');
+
+// Second Solution
+const stringCompression2 = (str) => {
+  let newStr = '';
+  let counter = 1;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      counter++;
+    }
+
+    if (str[i] !== str[i + 1]) {
+      newStr += str[i] + counter;
+      counter = 1;
+    }
+  }
+
+  if (newStr.length > str.length) {
+    return str;
+  }
+
+  return newStr;
+}
+
+stringCompression2('aabcccccaaa');
