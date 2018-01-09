8c830a96445b688ce63afe63aa46a808fb27b0a1
Maria Betances <mbetances1002@gmail.com>
2018-01-09 16:30:09 -0500

    update to stringRotations solution

--- Chapter1/stringRotations.js
+++ Chapter1/stringRotations.js
@@ -2,34 +2,41 @@
 // s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring (e.g.
 // 'waterbottle' is a rotation of 'erbottlewat')
 
-// O(n)
 const stringRotation = (s1, s2) => {
   let hashTable = {};
-  for (let i = 0; i < s1.length; i++) {
-    if (!hashTable[s1[i]]) {
-      hashTable[s1[i]] = 1;
+  let rotation = 0;
+  let s1Length = s1.length;
+
+  if (s1Length !== s2.length && s1Length === 0) {
+    return false;
+  }
+
+  for (let i = 0; i < s1Length; i++) {
+    if (hashTable[s1[i]]) {
+      let counter = hashTable[s1[i]];
+      counter++;
+      hashTable[s1[i]] = counter;
     } else {
-      let total = hashTable[s1[i]];
-      total++;
-      hashTable[s1[i]] = total;
+      hashTable[s1[i]] = 1;
+    }
+  }
+
+  for (let key in hashTable) {
+    if (hashTable[key] === 1) {
+      rotation = s2.indexOf(key);
+      break;
     }
   }
 
   for (let j = 0; j < s2.length; j++) {
-    if (hashTable[s2[j]]) {
-      let count = hashTable[s2[j]];
-      count--;
-      hashTable[s2[j]] = count;
-
-      if (count < 0) {
-        return false;
-      }
-    } else {
+    let letter = s1[j];
+    let newPosition = (j + rotation) % s1Length;
+    if (s2[newPosition] !== letter) {
       return false;
     }
   }
+
   return true;
 }
 
-// Test
 stringRotation('waterbottle', 'erbottlewat')
